4abec8ad613a1c84d1c7583e0a918be452fef22f
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-09-20 10:59:08 -0300

    update css modules
    Roadmap:
    Aprendi como criar o CSS inline direto no atributo, como variável JS e utilizando condicionais para mudar o estilo.
    Utilizar CSS e Sass no projeto;
    Vi como importar CSS e SASS no projeto é fácil dentro de um projeto criado com Create React App.
    Colocar o CSS Modules em um projeto com Create React App + Typescript;
    Configurei o projeto para aceitar CSS Modules.
    Vantagens de se utilizar CSS Modules.
    Discutimos as vantagens de se utilizar CSS Modules na aplicação.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./style.scss";
+import style from "./Botao.module.scss";
 
 class Botao extends React.Component {
   render() {
-    return <button className="botao">Botão</button>;
+    return <button className={style.botao}>Botão</button>;
   }
 }
 
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Botao from "../Botao";
-import "./style.scss";
+import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
@@ -15,7 +15,7 @@ class Formulario extends React.Component {
             placeholder="O que você quer estudar"
           />
         </div>
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
           <input
             type="time"
